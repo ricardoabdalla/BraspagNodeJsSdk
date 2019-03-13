c7000006c64580928f9960956ee757abe6650032
Maurici Ferreira Junior <m.f.j@hotmail.com>
2019-03-13 13:20:18 -0300

    Adição de função para checar vazios

---
+++ src/Common/Utilities.js
@@ -0,0 +1,17 @@
+module.exports = function isEmpty(val) {
+    let typeOfVal = typeof val;
+    switch(typeOfVal){
+        case 'object':
+            return (val.length == 0) || !Object.keys(val).length;
+            break;
+        case 'string':
+            let str = val.trim();
+            return str == '' || str == undefined;
+            break;
+        case 'number':
+            return val == '';
+            break;
+        default:
+            return val == '' || val == undefined;
+    }
+};
