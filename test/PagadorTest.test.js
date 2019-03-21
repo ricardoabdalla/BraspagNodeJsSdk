c6d5de29784f035d21d8cecffd372261a0546752
Maurici Ferreira Junior <m.f.j@hotmail.com>
2019-03-21 08:59:14 -0300

    Testes de captura, cancelamento, consulta e recorrência

--- test/PagadorTest.test.js
+++ test/PagadorTest.test.js
@@ -1,5 +1,6 @@
 const PagadorClient = require('../src/Pagador/PagadorClient');
 const TransactionStatus = require('../src/Common/TransactionsStatus');
+const RecurrencyInterval = require('../src/Common/RecurrencyInterval');
 const uuid = require('uuid/v1');
 
 var requestDataSet = () => {
@@ -372,7 +373,6 @@ describe('PagadorTests', () => {
     //#region MultiStep_Tests
 
     it('createSale_ThenCapture_ThenVoid_ThenGet', async () => {
-        
         const client = new PagadorClient({
             env: 'sandbox',
             credentials: {
@@ -416,6 +416,253 @@ describe('PagadorTests', () => {
         expect(getResponse.MerchantOrderId).not.toBeNull();
         expect(getResponse.Customer).not.toBeNull();
         expect(getResponse.Payment).not.toBeNull();
+    });
+
+    it('createSale_ThenCapture_ThenVoid_ThenGet', async () => {
+        const client = new PagadorClient({
+            env: 'sandbox',
+            credentials: {
+                    MerchantId: '33B6AC07-C48D-4F13-A5B9-D3516A378A0C', 
+                    MerchantKey: 'd6Rb3OParKvLfzNrURzwcT0f1lzNazS1o19yP6Y8'
+            }});
+
+        let request = requestDataSet();
+        
+        request.MerchantOrderId = uuid();
+            
+        let response = await client.createSale(request);
+
+        expect(response.Payment.Status).toBe(TransactionStatus.Authorized);
+        expect(response.httpStatus).toBe(201);
+
+        let getResponse = await client.getOrderById(response.Payment.PaymentId);
+
+        expect(getResponse.httpStatus).toBe(200);
+        expect(getResponse.Payment).not.toBeNull();
+        expect(getResponse.Payment.PaymentId).not.toBeNull();
+
+    });
+
+    //#endregion
+
+    //#region Recurrent
+
+    it('ChangeRecurrencyCustomer_ReturnsOk', async () => {
+        const client = new PagadorClient({
+            env: 'sandbox',
+            credentials: {
+                    MerchantId: '33B6AC07-C48D-4F13-A5B9-D3516A378A0C', 
+                    MerchantKey: 'd6Rb3OParKvLfzNrURzwcT0f1lzNazS1o19yP6Y8'
+            }});
+
+        let request = requestDataSet();
+        
+        request.MerchantOrderId = uuid();
+
+        var threeMonthsLater = new Date();
+        threeMonthsLater.setMonth(threeMonthsLater.getMonth() + 3);
+
+        request.Payment.RecurrentPayment = {
+            AuthorizeNow: true,
+            EndDate: threeMonthsLater.toISOString().slice(0, 10),
+            Interval: 'Monthly'
+        };
+          
+        let response = await client.createSale(request);
+
+        expect(response.Payment.Status).toBe(TransactionStatus.Authorized);
+        expect(response.httpStatus).toBe(201);
+        expect(response.Payment.RecurrentPayment).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.RecurrentPaymentId).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.NextRecurrency).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.Interval).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.EndDate).not.toBeNull();
+    });
+
+    it('ChangeRecurrencyEndDate_ReturnsOk', async () => {
+        const client = new PagadorClient({
+            env: 'sandbox',
+            credentials: {
+                    MerchantId: '33B6AC07-C48D-4F13-A5B9-D3516A378A0C', 
+                    MerchantKey: 'd6Rb3OParKvLfzNrURzwcT0f1lzNazS1o19yP6Y8'
+            }});
+
+        let request = requestDataSet();
+        
+        request.MerchantOrderId = uuid();
+
+        var threeMonthsLater = new Date();
+        threeMonthsLater.setMonth(threeMonthsLater.getMonth() + 3);
+
+        request.Payment.RecurrentPayment = {
+            AuthorizeNow: true,
+            EndDate: threeMonthsLater.toISOString().slice(0, 10),
+            Interval: 'Monthly'
+        };
+          
+        let response = await client.createSale(request);
+
+        expect(response.Payment.Status).toBe(TransactionStatus.Authorized);
+        expect(response.httpStatus).toBe(201);
+        expect(response.Payment.RecurrentPayment).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.RecurrentPaymentId).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.NextRecurrency).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.Interval).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.EndDate).not.toBeNull();
+
+        let endDate = new Date();
+        endDate.setMonth(endDate.getMonth() + 3);
+
+        let recurrentResponse = await client.changeRecurrencyEndDate(response.Payment.RecurrentPayment.RecurrentPaymentId, endDate.toISOString().slice(0, 10));
+
+        expect(recurrentResponse.httpStatus).toBe(200);
+    });
+
+    it('ChangeRecurrencyInterval_ReturnsOk', async () => {
+        const client = new PagadorClient({
+            env: 'sandbox',
+            credentials: {
+                    MerchantId: '33B6AC07-C48D-4F13-A5B9-D3516A378A0C', 
+                    MerchantKey: 'd6Rb3OParKvLfzNrURzwcT0f1lzNazS1o19yP6Y8'
+            }});
+
+        let request = requestDataSet();
+        
+        request.MerchantOrderId = uuid();
+
+        var threeMonthsLater = new Date();
+        threeMonthsLater.setMonth(threeMonthsLater.getMonth() + 3);
+
+        request.Payment.RecurrentPayment = {
+            AuthorizeNow: true,
+            EndDate: threeMonthsLater.toISOString().slice(0, 10),
+            Interval: 'Monthly'
+        };
+          
+        let response = await client.createSale(request);
+
+        expect(response.Payment.Status).toBe(TransactionStatus.Authorized);
+        expect(response.httpStatus).toBe(201);
+        expect(response.Payment.RecurrentPayment).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.RecurrentPaymentId).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.NextRecurrency).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.Interval).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.EndDate).not.toBeNull();
+
+        let recurrentResponse = await client.changeRecurrencyInterval(response.Payment.RecurrentPayment.RecurrentPaymentId, RecurrencyInterval.Quarterly);
+
+        expect(recurrentResponse.httpStatus).toBe(200);
+    });
+
+    it('ChangeRecurrencyDay_ReturnsOk', async () => {
+        const client = new PagadorClient({
+            env: 'sandbox',
+            credentials: {
+                    MerchantId: '33B6AC07-C48D-4F13-A5B9-D3516A378A0C', 
+                    MerchantKey: 'd6Rb3OParKvLfzNrURzwcT0f1lzNazS1o19yP6Y8'
+            }});
+
+        let request = requestDataSet();
+        
+        request.MerchantOrderId = uuid();
+
+        var threeMonthsLater = new Date();
+        threeMonthsLater.setMonth(threeMonthsLater.getMonth() + 3);
+
+        request.Payment.RecurrentPayment = {
+            AuthorizeNow: true,
+            EndDate: threeMonthsLater.toISOString().slice(0, 10),
+            Interval: 'Monthly'
+        };
+          
+        let response = await client.createSale(request);
+
+        expect(response.Payment.Status).toBe(TransactionStatus.Authorized);
+        expect(response.httpStatus).toBe(201);
+        expect(response.Payment.RecurrentPayment).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.RecurrentPaymentId).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.NextRecurrency).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.Interval).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.EndDate).not.toBeNull();
+
+        let recurrentResponse = await client.changeRecurrencyInterval(response.Payment.RecurrentPayment.RecurrentPaymentId, 10);
+
+        expect(recurrentResponse.httpStatus).toBe(200);
+    });
+
+    it('ChangeRecurrencyAmount_ReturnsOk', async () => {
+        const client = new PagadorClient({
+            env: 'sandbox',
+            credentials: {
+                    MerchantId: '33B6AC07-C48D-4F13-A5B9-D3516A378A0C', 
+                    MerchantKey: 'd6Rb3OParKvLfzNrURzwcT0f1lzNazS1o19yP6Y8'
+            }});
+
+        let request = requestDataSet();
+        
+        request.MerchantOrderId = uuid();
+
+        var threeMonthsLater = new Date();
+        threeMonthsLater.setMonth(threeMonthsLater.getMonth() + 3);
+
+        request.Payment.RecurrentPayment = {
+            AuthorizeNow: true,
+            EndDate: threeMonthsLater.toISOString().slice(0, 10),
+            Interval: 'Monthly'
+        };
+          
+        let response = await client.createSale(request);
+
+        expect(response.Payment.Status).toBe(TransactionStatus.Authorized);
+        expect(response.httpStatus).toBe(201);
+        expect(response.Payment.RecurrentPayment).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.RecurrentPaymentId).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.NextRecurrency).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.Interval).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.EndDate).not.toBeNull();
+
+        let recurrentResponse = await client.changeRecurrencyInterval(response.Payment.RecurrentPayment.RecurrentPaymentId, 15000);
+
+        expect(recurrentResponse.httpStatus).toBe(200);
+    });
+
+    it('ChangeRecurrencyNextPaymentDate_ReturnsOk', async () => {
+        const client = new PagadorClient({
+            env: 'sandbox',
+            credentials: {
+                    MerchantId: '33B6AC07-C48D-4F13-A5B9-D3516A378A0C', 
+                    MerchantKey: 'd6Rb3OParKvLfzNrURzwcT0f1lzNazS1o19yP6Y8'
+            }});
+
+        let request = requestDataSet();
+        
+        request.MerchantOrderId = uuid();
+
+        var threeMonthsLater = new Date();
+        threeMonthsLater.setMonth(threeMonthsLater.getMonth() + 3);
+
+        request.Payment.RecurrentPayment = {
+            AuthorizeNow: true,
+            EndDate: threeMonthsLater.toISOString().slice(0, 10),
+            Interval: 'Monthly'
+        };
+          
+        let response = await client.createSale(request);
+
+        expect(response.Payment.Status).toBe(TransactionStatus.Authorized);
+        expect(response.httpStatus).toBe(201);
+        expect(response.Payment.RecurrentPayment).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.RecurrentPaymentId).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.NextRecurrency).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.Interval).not.toBeNull();
+        expect(response.Payment.RecurrentPayment.EndDate).not.toBeNull();
+
+        let nextPaymentDate = new Date();
+        nextPaymentDate.setMonth(nextPaymentDate.getMonth() + 3);
+
+        let recurrentResponse = await client.changeRecurrencyEndDate(response.Payment.RecurrentPayment.RecurrentPaymentId, nextPaymentDate.toISOString().slice(0, 10));
+
+        expect(recurrentResponse.httpStatus).toBe(200);
     });
 
     //#endregion
